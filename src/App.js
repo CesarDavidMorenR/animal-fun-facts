1a17d223cce94e3e69e40c55d63bc8afd765c92e
cesar david moreno <cesardaviddev@gmail.com>
2022-05-07 13:16:06 +0200

    Avanze en sustitucion de elementos y actualizando codigo de codeacademy a la nueave version en conde

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { animals } from "./cou/animals.js";
+import './cou/styles.css';
 
 function App() {
+
+  const title = "";
+
+   const background = (
+     <img className="background" alt="ocean" src="./cou/assets/OLP.jpg" />
+   );
+
+  const displayFact = (e) => {
+    const animalInfo = e.target.alt;
+    const selectedAnimal = animals[animalInfo];
+    const randIndex = Math.floor(Math.random() * selectedAnimal.facts.length);
+    const funFact = selectedAnimal.facts[randIndex];
+
+    document.getElementById("fact").innerHTML = funFact;
+  };
+
+  const images = [];
+  for (let animal in animals) {
+    images.push(
+      <img
+        key={animal}
+        className="animal"
+        alt={animal}
+        src={animals[animal].image}
+        aria-label={animal}
+        role="button"
+        onClick={displayFact}
+      />
+    );
+  }
+
+  const showBackground = true;
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and sModified
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      {showBackground === true ? background : images}
+      <h1>{title === "" ? "Click an animal for fun facts!" : title}</h1>
+      <div className="animals">
+        <p id="fact"></p>
+        {images}
+      </div>
     </div>
   );
 }
